1fe0d2159615de4ef255b5ed1f74278eb738e52b
AI Now <oli.mpele@gmail.com>
2014-07-15 18:36:50 -0400

    clean build before compilation and check for success

--- make.py
+++ make.py
@@ -1,9 +1,18 @@
 import os
+import os.path
 
 print "Synth Build Tool"
 
+os.system ("rm -f bin/synth")
+
 os.system ("gcc -o bin/synth -include synth.h synth.c */*.c */*/*.c")
 
-print "Running Test Suite"
+if os.path.exists ("bin/synth"):
 
-os.system ("bin/synth")
+	print "Running Test Suite"
+
+	os.system ("bin/synth")
+
+else:
+
+	print "Build Failed"
