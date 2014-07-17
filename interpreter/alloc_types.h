9857225727d39f5e19f25299bbd06fda4758cc56
AI Now <oli.mpele@gmail.com>
2014-07-17 16:36:22 -0400

    sketch allocator types

---
+++ interpreter/alloc_types.h
@@ -0,0 +1,14 @@
+#ifndef __SYNTH_TYPES__
+#define __SYNTH_TYPES__
+
+#define type_synth_list_t 10
+#define type_synth_pool_t 11
+#define type_synth_map_t 12
+#define type_synth_hashmap_t 13
+
+#define type_synth_graph_node_t 20
+#define type_synth_graph_link_t 21
+
+extern string types[];
+
+#endif
--- synth.h
+++ synth.h
@@ -4,6 +4,7 @@
 
 #include "utils/common.h"
 #include "interpreter/alloc.h"
+#include "interpreter/alloc_types.h"
 #include "utils/test.h"
 #include "utils/list.h"
 #include "interpreter/graph.h"
