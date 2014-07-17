bffdc14aa4279cf33d29602fcd34c12a6b3cd45f
AI Now <oli.mpele@gmail.com>
2014-07-17 12:57:05 -0400

    add some common defines

--- utils/common.h
+++ utils/common.h
@@ -1,6 +1,12 @@
 #ifndef __SYNTH_UTILS__
 #define __SYNTH_UTILS__
 
+#define false 0
+#define  true 1
+
+#define string char*
+#define pointer void*
+
 #define sweep(buf) memset(buf, 0, sizeof(buf));
 
 #endif
