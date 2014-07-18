92dc790f8f44c2d1c83d3e7c938d41fb1e3cbc78
AI Now <oli.mpele@gmail.com>
2014-07-18 12:40:27 -0400

    make

--- make.py
+++ make.py
@@ -7,7 +7,9 @@ print "Synth Build Tool"
 
 os.system ("rm -f bin/synth")
 
-os.system ("gcc -o bin/synth -lm -include synth.h synth.c */*.c */*/*.c")
+# os.system ("gcc -o bin/synth -lm -include synth.h synth.c */*.c */*/*.c")
+
+os.system ("make -f tools/make/Makefile")
 
 if os.path.exists ("bin/synth"):
 
