3ada5309a0aeeb778d8b2735bfc902ab1420245c
AI Now <oli.mpele@gmail.com>
2014-07-19 13:48:52 -0400

    add a newline to fail() output

--- utils/test.h
+++ utils/test.h
@@ -6,13 +6,14 @@ static int test_number;
 static int planned_tests;
 #define ok(result, description) \
 	test_number++; \
-	if (result) printf("ok, %i\n", test_number); else printf("fail, %i: %s", test_number, #description);
+	if (result) printf("ok, %i\n", test_number); else printf("fail, %i: %s\n", test_number, #description);
 #define fail(result, description) ok(!(result), description)
 #define plan(num, title) planned_tests = num; printf("Planned %i tests for %s..\n", num, #title);
 
 // test suite
 void test_suite ();
 void test_sanity ();
+void test_alloc ();
 void test_list ();
 
 #endif
