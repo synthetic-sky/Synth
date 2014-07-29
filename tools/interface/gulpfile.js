d053a37185c725cc3230d0890bf027e4f8edf4f7
AI Now <oli.mpele@gmail.com>
2014-07-29 17:32:50 +0000

    work around a random hangup

--- tools/interface/gulpfile.js
+++ tools/interface/gulpfile.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var gulp = require ('gulp');
 var plumber = require ('gulp-plumber')
 var regenerator = require ('gulp-regenerator');
@@ -9,6 +10,13 @@ var client_path_repl = '$1/dist$2';
 
 gulp.task ('default', ['watch']);
 
+gulp.task ('copy', function () {
+  gulp.src ('index.html')
+      .pipe (replace (client_path_re, client_path_repl))
+      .pipe (gulp.dest ('dist'));
+});
+
+
 gulp.task ('watch', function () {
 
   watch ({ glob: 'client/**/*.js' })
@@ -17,10 +25,12 @@ gulp.task ('watch', function () {
       .pipe (replace (client_path_re, client_path_repl))
       .pipe (gulp.dest ('dist'));
 
+  watch ({ glob: 'index.html' }, function () {
+    require ('child_process') .exec ("gulp copy");
+  });
+
   watch ({ glob: 'client/**/*.css' })
     .pipe (gulp.dest ('dist'));
-
-  watch ({ glob: '*.html' })
-    .pipe (replace (client_path_re, client_path_repl))
-    .pipe (gulp.dest ('dist'));
 });
+
+
